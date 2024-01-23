73353d185ce7d36e22514b892596df111457a6a5
EJay Enun <68190790+EJ-enun@users.noreply.github.com>
2024-01-23 15:44:29 +0100

    Add files via upload

--- application/dash.py
+++ application/dash.py
@@ -12,23 +12,8 @@ server = app.server
 #App Title.
 app.title = 'ENUN BASSEY'
 # Navbar
-navbar = dbc.Nav(className="nav nav-pills", children=[
-    ## logo/home
-    dbc.NavItem(html.Img(src=app.get_asset_url("logo.PNG"), height="60px")),
-    ## about
-    dbc.NavItem(html.Div([
-        dbc.NavLink("About", href="/", id="about-popover", active=False),
-        dbc.Popover(id="about", is_open=False, target="about-popover", children=[
-            dbc.PopoverHeader("How it works"), dbc.PopoverBody("Some stuff here")
-        ])
-    ])),
-    ## links
-    dbc.DropdownMenu(label="Links", nav=True, children=[
-        dbc.DropdownMenuItem([html.I(className="fa fa-linkedin"), "  Contacts"], href=config.contacts, target="_blank"), 
-        dbc.DropdownMenuItem([html.I(className="fa fa-github"), "  Code"], href=config.code, target="_blank")
-    ])
-])
-navbar_2 = dbc.NavbarSimple(
+
+navbar = dbc.NavbarSimple(
     children=[
         dbc.NavItem(dbc.NavLink("Page 1", href="#")),
         dbc.DropdownMenu(
@@ -47,7 +32,7 @@ navbar_2 = dbc.NavbarSimple(
     color="primary",
     dark=True,
 )
-app.layout = dbc.Container(fluid=True, children=[navbar, html.Br(), navbar_2])
+app.layout = dbc.Container(fluid=True, children=[navbar])
 
 @app.callback(
     [Output('some_stuff', 'children'),],
