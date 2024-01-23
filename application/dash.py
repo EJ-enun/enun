b403a1f16bd2d84052a1ac1b3d05d665747d7aa2
EJay Enun <68190790+EJ-enun@users.noreply.github.com>
2024-01-23 15:22:57 +0100

    Add files via upload

--- application/dash.py
+++ application/dash.py
@@ -9,8 +9,45 @@ app = dash.Dash(__name__,
                 external_stylesheets=[dbc.themes.QUARTZ])
 
 server = app.server
-app.title = 'Title'
-app.layout = dbc.Container(fluid=True, children=[])
+#App Title.
+app.title = 'ENUN BASSEY'
+# Navbar
+navbar = dbc.Nav(className="nav nav-pills", children=[
+    ## logo/home
+    dbc.NavItem(html.Img(src=app.get_asset_url("logo.PNG"), height="60px")),
+    ## about
+    dbc.NavItem(html.Div([
+        dbc.NavLink("About", href="/", id="about-popover", active=False),
+        dbc.Popover(id="about", is_open=False, target="about-popover", children=[
+            dbc.PopoverHeader("How it works"), dbc.PopoverBody(about.txt)
+        ])
+    ])),
+    ## links
+    dbc.DropdownMenu(label="Links", nav=True, children=[
+        dbc.DropdownMenuItem([html.I(className="fa fa-linkedin"), "  Contacts"], href=config.contacts, target="_blank"), 
+        dbc.DropdownMenuItem([html.I(className="fa fa-github"), "  Code"], href=config.code, target="_blank")
+    ])
+])
+navbar_2 = dbc.NavbarSimple(
+    children=[
+        dbc.NavItem(dbc.NavLink("Page 1", href="#")),
+        dbc.DropdownMenu(
+            children=[
+                dbc.DropdownMenuItem("More pages", header=True),
+                dbc.DropdownMenuItem("Page 2", href="#"),
+                dbc.DropdownMenuItem("Page 3", href="#"),
+            ],
+            nav=True,
+            in_navbar=True,
+            label="More",
+        ),
+    ],
+    brand="NavbarSimple",
+    brand_href="#",
+    color="primary",
+    dark=True,
+)
+app.layout = dbc.Container(fluid=True, children=[navbar, html.Br(), navbar_2])
 
 @app.callback(
     [Output('some_stuff', 'children'),],
